6c3cf362181204c248c52918785001eaf4518cb9
Vane <123445952+VaneLP@users.noreply.github.com>
2023-11-14 12:01:27 +0100

    css

--- practica1_u4/src/main/java/com/example/practica1_u4/HelloApplication.java
+++ practica1_u4/src/main/java/com/example/practica1_u4/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         //para agregar el css
-        //Application.setUserAgentStylesheet(STYLESHEET_CASPIAN);
+        Application.setUserAgentStylesheet(String.valueOf(HelloApplication.class.getResource("dracula.css")));
         //leemos el FXML
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         //redimensionamos la ventana
