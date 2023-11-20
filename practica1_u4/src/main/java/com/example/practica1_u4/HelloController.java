6454b2f542404b835887aec3ef283a5851bfbe17
Vane <123445952+VaneLP@users.noreply.github.com>
2023-11-20 11:58:02 +0100

    menupersona funcional

--- practica1_u4/src/main/java/com/example/practica1_u4/HelloController.java
+++ practica1_u4/src/main/java/com/example/practica1_u4/HelloController.java
@@ -1,10 +1,11 @@
 package com.example.practica1_u4;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.*;
 import javafx.stage.Popup;
 
 import java.net.URL;
@@ -59,6 +60,8 @@ public class HelloController implements Initializable {
 
     @FXML
     private Menu menuProfe;
+    @FXML
+    private Menu menuPer;
 
     @FXML
     private TableView<Persona> tablaPersona;
@@ -75,23 +78,98 @@ public class HelloController implements Initializable {
     @FXML
     private TextField txtTlf;
 
+
     // ---- LISTENERS ----
     // Boton agregar
     @FXML
     void pulsarAgregar(MouseEvent event) {
-        //agregamos a la lista una nueva persona
-        listaPersonas.add(new Persona(txtNombre.getText(),txtDni.getText(),txtTlf.getText(),textEdad.getText()));
-
-        txtNombre.setText("");
-        txtDni.setText("");
-        txtTlf.setText("");
-        textEdad.setText("");
+        agregarPer();
     }
 
     // Boton buscar
     @FXML
     void pulsarBuscar(MouseEvent event) {
+        buscarPer();
+    }
 
+    // Boton eliminar
+    @FXML
+    void pulsarEliminar(MouseEvent event) {
+        eliminarPer();
+    }
+
+    // Boton mostrar
+    @FXML
+    void pulsarMostrar(MouseEvent event) {
+        mostrarPer();
+    }
+
+    @FXML
+    void itemAgregarPer(ActionEvent event) {
+        agregarPer();
+    }
+
+    @FXML
+    void itemBuscarPer(ActionEvent event) {
+        buscarPer();
+    }
+
+    @FXML
+    void itemEliminarPer(ActionEvent event) {
+        eliminarPer();
+    }
+
+    @FXML
+    void itemMostrarPer(ActionEvent event) {
+        mostrarPer();
+    }
+
+    // ---- METODOS ---
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("Nombre"));
+        colDNI.setCellValueFactory(new PropertyValueFactory<>("DNI"));
+        colTlf.setCellValueFactory(new PropertyValueFactory<>("Tlf"));
+        colEdad.setCellValueFactory(new PropertyValueFactory<>("Edad"));
+    }
+
+    /**
+     * Metodo para mostrar en la tabla las personas
+     */
+    private void mostrarPer() {
+        //eliminamos todos los items de la tabla
+        tablaPersona.getItems().removeAll(tablaPersona.getItems());
+
+        //refrescamos la tabla
+        tablaPersona.refresh();
+
+        //añadimnos a las personas en la tabla
+        for (Persona p : listaPersonas) {
+            tablaPersona.getItems().add(p);
+        }
+    }
+
+    /**
+     * Metodo para eliminar personas cuando seleccionamos una fila en la tabla
+     */
+    private void eliminarPer() {
+        //eliminamos de la tabla el item que tengamos seleccionado
+        Persona p = tablaPersona.getSelectionModel().getSelectedItem();
+
+        //lo eliminamos de la lista
+        listaPersonas.remove(p);
+
+        //eliminamos todos los items de la tabla
+        tablaPersona.getItems().remove(p);
+
+        //refrescamos la tabla
+        tablaPersona.refresh();
+    }
+
+    /**
+     * Metodo para buscar personas a traves de su DNI
+     */
+    private void buscarPer() {
         //recorremos la lista de personas
         for (Persona p : listaPersonas) {
             //si el dni de la persona es igual al dni del textfield
@@ -107,45 +185,18 @@ public class HelloController implements Initializable {
         tablaPersona.refresh();
     }
 
-    // Boton eliminar
-    @FXML
-    void pulsarEliminar(MouseEvent event) {
-        //eliminamos de la tabla el item que tengamos seleccionado
-        Persona p = tablaPersona.getSelectionModel().getSelectedItem();
+    /**
+     * Metodo para agregar personas
+     */
+    private void agregarPer() {
+        //agregamos a la lista una nueva persona
+        listaPersonas.add(new Persona(txtNombre.getText(),txtDni.getText(),txtTlf.getText(),textEdad.getText()));
 
-        //lo eliminamos de la lista
-        listaPersonas.remove(p);
-
-        //eliminamos todos los items de la tabla
-        tablaPersona.getItems().remove(p);
-
-        //refrescamos la tabla
-        tablaPersona.refresh();
-
+        txtNombre.setText("");
+        txtDni.setText("");
+        txtTlf.setText("");
+        textEdad.setText("");
     }
 
-    // Boton mostrar
-    @FXML
-    void pulsarMostrar(MouseEvent event) {
-        //eliminamos todos los items de la tabla
-        tablaPersona.getItems().removeAll(tablaPersona.getItems());
-
-        //refrescamos la tabla
-        tablaPersona.refresh();
-
-        //añadimnos a las personas en la tabla
-        for (Persona p : listaPersonas) {
-            tablaPersona.getItems().add(p);
-        }
-    }
-
-    // ---- METODOS ---
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        colNombre.setCellValueFactory(new PropertyValueFactory<>("Nombre"));
-        colDNI.setCellValueFactory(new PropertyValueFactory<>("DNI"));
-        colTlf.setCellValueFactory(new PropertyValueFactory<>("Tlf"));
-        colEdad.setCellValueFactory(new PropertyValueFactory<>("Edad"));
-    }
 }
 
